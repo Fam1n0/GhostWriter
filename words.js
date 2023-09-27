311ee20aa67c758aa9f4049000b4159f1fb1c563
Fam1n0 <142743561+Fam1n0@users.noreply.github.com>
2023-09-26 21:58:41 -0400

    Add files via upload

---
+++ words.js
@@ -0,0 +1,28 @@
+// words.js
+var mysticWords = [
+    // Common Words & Connectors
+    "a", "it", "the", "though", "and", "as", "well", "as", "but", "if", "then", "for", "in", "with", "by", "from", "to", "is", "are", "were", "will", "yet", "or", "of",
+
+    // Judaic
+    "Kabbalah", "Talmud", "Mitzvah", "Yeshiva", "Sephirot", "Chesed", "Gevurah", "Tiferet", "Malkuth", "Shekhinah",
+    
+    // Christian
+    "Trinity", "Salvation", "Eucharist", "Apostle", "Epistle", "Evangelist", "Resurrection", "Sacrament", "Theology", "Eschatology",
+    
+    // Philosophical
+    "Existentialism", "Epistemology", "Metaphysics", "Utilitarianism", "Nihilism", "Dialectic", "Empiricism", "Aesthetics", "Ontology", "Positivism",
+    
+    // Psychological
+    "Ego", "Id", "Superego", "Cognition", "Subconscious", "Archetype", "Neurosis", "Psyche", "Transference", "Catharsis",
+    
+    // Taoist
+    "Tao", "Yin", "Yang", "Wu Wei", "Qi", "Ziran", "Daojia", "Laozi", "I Ching", "Feng Shui",
+    
+    // Buddhist
+    "Dharma", "Karma", "Samsara", "Nirvana", "Bodhisattva", "Sangha", "Mandala", "Vajra", "Tantra", "Zen",
+    
+    // Prophetic Enhancers
+    "shall", "proclaim", "foreseen", "behold", "rise", "fall", "eternity", "transgress", "redeem", "vision", "fate", "destiny", "omens", "herald", "awaken", "journey", "seek", "find", "beyond", "realm", "mystic", "echoes", "time"
+];
+
+// As always, you can add, modify, or remove words to tailor the list to your liking.
